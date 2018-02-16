8204dee22e741b69ff87a9b38da2e2db6216a3a9
Christopher Luong <Christopher@luong-mac.local>
2018-02-16 10:21:19 -0600

    Initial Commit

---
+++ Person UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Christopher Luong on 2/16/18.
+//  Copyright © 2018 Christopher Luong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
